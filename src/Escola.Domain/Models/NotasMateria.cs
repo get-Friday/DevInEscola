ab17755cb72a773583cd821de5433d2675680ca5
Gabriel "Friday" Thomas <gabriel.getdev@gmail.com>
2022-08-31 16:17:44 -0300

    Feature: Cria objeto NotasMateria

---
+++ src/Escola.Domain/Models/NotasMateria.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Escola.Domain.Models
+{
+    internal class NotasMateria
+    {
+        public Guid Id { get; set; } = new Guid();
+        public Guid BoletimId { get; set; }
+        public Guid MateriaId { get; set; }
+        public int Nota { get; set; }
+    }
+}
