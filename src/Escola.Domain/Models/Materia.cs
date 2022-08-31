6b8cc569e77d96272b2752f9f5e56ac738aaed7e
Gabriel "Friday" Thomas <gabriel.getdev@gmail.com>
2022-08-31 16:17:57 -0300

    Feature: Cria objeto Materia

---
+++ src/Escola.Domain/Models/Materia.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Escola.Domain.Models
+{
+    internal class Materia
+    {
+        public Guid Id { get; set; } = new Guid();
+        public string Nome { get; set; }
+    }
+}
