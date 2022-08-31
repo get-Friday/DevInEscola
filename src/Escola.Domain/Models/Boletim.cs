4b99f4bc9c279469294a25dc22c608f983397b02
Gabriel "Friday" Thomas <gabriel.getdev@gmail.com>
2022-08-31 16:13:39 -0300

    Feature: Cria objeto Boletim

---
+++ src/Escola.Domain/Models/Boletim.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Escola.Domain.Models
+{
+    internal class Boletim
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public Guid AlunoId { get; set; }
+    }
+}
