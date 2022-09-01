dcc7466ef9c5c261d7f700ec8f7647dd0e92b515
Gabriel "Friday" Thomas <gabriel.getdev@gmail.com>
2022-09-01 15:07:46 -0300

    Feature: Mapeia entidade Materia

---
+++ src/Escola.Infra/DataBase/Mappings/MateriaMap.cs
@@ -0,0 +1,30 @@
+﻿using Escola.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Escola.Infra.DataBase.Mappings
+{
+    public class MateriaMap : IEntityTypeConfiguration<Materia>
+    {
+        public void Configure(EntityTypeBuilder<Materia> builder)
+        {
+            builder.ToTable("MATERIA");
+
+            builder.HasKey(m => m.Id)
+                .HasName("PK_MateriaID");
+
+            builder.Property(m => m.Id)
+                .HasColumnName("ID")
+                .HasColumnType("uniqueidentifier");
+
+            builder.Property(m => m.Nome)
+                .HasColumnName("NOME")
+                .HasColumnType("VARCHAR");
+        }
+    }
+}
