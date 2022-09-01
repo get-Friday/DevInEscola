89ab528b3692639d866b370a623e2e13bba162be
Gabriel "Friday" Thomas <gabriel.getdev@gmail.com>
2022-09-01 14:58:08 -0300

    Feature: Mapeia entidade NotasMateria

---
+++ src/Escola.Infra/DataBase/Mappings/NotasMateriaMap.cs
@@ -0,0 +1,34 @@
+﻿using Escola.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Escola.Infra.DataBase.Mappings
+{
+    public class NotasMateriaMap : IEntityTypeConfiguration<NotasMateria>
+    {
+        public void Configure(EntityTypeBuilder<NotasMateria> builder)
+        {
+            builder.ToTable("NOTAS_MATERIA");
+
+            builder.HasKey(nt => nt.Id)
+                .HasName("PK_NotasMateriaID");
+
+            builder.Property(nt => nt.Id)
+                .HasColumnName("ID")
+                .HasColumnType("uniqueidentifier");
+
+            builder.Property(nt => nt.Nota)
+                .HasColumnName("NOTA")
+                .HasColumnType("int");
+
+            builder.HasOne<Boletim>(nt => nt.Boletim)
+                .WithMany(b => b.NotasMateria)
+                .HasForeignKey(nt => nt.BoletimId);
+        }
+    }
+}
