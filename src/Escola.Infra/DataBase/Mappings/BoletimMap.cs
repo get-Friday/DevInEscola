4c4f43628f02b689aec8852b0ee83228c82e5b7b
Gabriel "Friday" Thomas <gabriel.getdev@gmail.com>
2022-09-01 12:10:24 -0300

    Feature: Mapeia entidade boletins

---
+++ src/Escola.Infra/DataBase/Mappings/BoletimMap.cs
@@ -0,0 +1,34 @@
+﻿using Escola.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Escola.Infra.DataBase.Mappings
+{
+    public class BoletimMap : IEntityTypeConfiguration<Boletim>
+    {
+        public void Configure(EntityTypeBuilder<Boletim> builder)
+        {
+            builder.ToTable("BOLETIM");
+
+            builder.HasKey(b => b.Id)
+                .HasName("PK_BoletimID");
+
+            builder.Property(b => b.Id)
+                .HasColumnName("ID")
+                .HasColumnType("uniqueidentifier");
+
+            builder.Property(b => b.AlunoId)
+                .HasColumnName("AlunoID")
+                .HasColumnType("uniqueidentifier");
+
+            builder.HasOne<Aluno>(b => b.Aluno)
+                .WithMany(a => a.Boletins)
+                .HasForeignKey(b => b.AlunoId);
+        }
+    }
+}
