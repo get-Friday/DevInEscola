65a7f30b998998128f29325f51560e580ffc3aab
Gabriel "Friday" Thomas <gabriel.getdev@gmail.com>
2022-09-02 14:21:22 -0300

    Feature: cria arquivo MateriaController

---
+++ src/Escola.Api/Controllers/MateriasController.cs
@@ -0,0 +1,16 @@
+﻿using Escola.Domain.Interfaces.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Escola.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MateriasController : ControllerBase
+    {
+        private readonly IMateriaServico _materiaServico;
+        public MateriasController(IMateriaServico materiaServico)
+        {
+            _materiaServico = materiaServico;
+        }
+    }
+}
