5509f23ddfa42dce5ad5192fe600a9df5f362976
Gabriel "Friday" Thomas <gabriel.getdev@gmail.com>
2022-09-02 14:21:36 -0300

    Feature: Cria DTO para Materia

---
+++ src/Escola.Domain/DTO/MateriaDTO.cs
@@ -0,0 +1,23 @@
+﻿using Escola.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Escola.Domain.DTO
+{
+    public class MateriaDTO
+    {
+        public MateriaDTO()
+        {
+        }
+        public MateriaDTO(Materia materia)
+        {
+            Id = materia.Id;
+            Nome = materia.Nome;
+        }
+        public Guid Id { get; set; }
+        public string Nome { get; set; }
+    }
+}
