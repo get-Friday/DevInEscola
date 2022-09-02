9792e84a3e6eb03d391ed42990117a80bd9232a7
Gabriel "Friday" Thomas <gabriel.getdev@gmail.com>
2022-09-02 14:20:41 -0300

    Feature: Cria repositorio para Materia

---
+++ src/Escola.Infra/DataBase/Repositories/MateriaRepositorio.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Escola.Domain.Interfaces.Repositories;
+using Escola.Domain.Models;
+
+namespace Escola.Infra.DataBase.Repositories
+{
+    public class MateriaRepositorio : IMateriaRepositorio
+    {
+        private readonly EscolaDBContexto _contexto;
+        public MateriaRepositorio(EscolaDBContexto contexto)
+        {
+            _contexto = contexto;
+        }
+
+        public IEnumerable<Materia> ObterTodos()
+        {
+            throw new NotImplementedException();
+        }
+        public Materia ObterPorId(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+        public void Inserir(Materia materia)
+        {
+            throw new NotImplementedException();
+        }
+        public void Excluir(Materia materia)
+        {
+            throw new NotImplementedException();
+        }
+        public void Alterar(Materia materia)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
