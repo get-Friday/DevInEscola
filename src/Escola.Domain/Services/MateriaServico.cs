3597de8fa7a47b6be11af05d7618e99171d93c37
Gabriel "Friday" Thomas <gabriel.getdev@gmail.com>
2022-09-02 14:20:55 -0300

    Feature: cria servico para materia

---
+++ src/Escola.Domain/Services/MateriaServico.cs
@@ -0,0 +1,40 @@
+﻿using Escola.Domain.DTO;
+using Escola.Domain.Interfaces.Repositories;
+using Escola.Domain.Interfaces.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Escola.Domain.Services
+{
+    public class MateriaServico : IMateriaServico
+    {
+        private readonly IMateriaRepositorio _materiaRepositorio;
+        public MateriaServico(IMateriaRepositorio materiaRepositorio)
+        {
+            _materiaRepositorio = materiaRepositorio;
+        }
+        public IList<MateriaDTO> ObterTodos()
+        {
+            throw new NotImplementedException();
+        }
+        public MateriaDTO ObterPorId(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+        public void Inserir(MateriaDTO materia)
+        {
+            throw new NotImplementedException();
+        }
+        public void Excluir(MateriaDTO materia)
+        {
+            throw new NotImplementedException();
+        }
+        public void Alterar(MateriaDTO materia)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
