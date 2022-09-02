a6fb1dbe610818d4c30f1184fd3e06926fff5450
Gabriel "Friday" Thomas <gabriel.getdev@gmail.com>
2022-09-02 20:55:09 -0300

    Feature: Implementa Middleware para tratar erros

---
+++ src/Escola.Api/Config/ErrorMiddleware.cs
@@ -0,0 +1,49 @@
+﻿using Escola.Domain.DTO;
+using Escola.Domain.Exceptions;
+using System.Net;
+
+namespace Escola.Api.Config
+{
+    public class ErrorMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ErrorMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync (HttpContext httpContext)
+        {
+            try
+            {
+                await _next(httpContext);
+            }
+            catch (Exception ex)
+            {
+                await TratamentoExcecao(httpContext, ex);
+            }
+        }
+        private Task TratamentoExcecao(HttpContext context, Exception exception)
+        {
+            HttpStatusCode status;
+            string message;
+
+            switch (exception)
+            {
+                case DuplicadoException:
+                    status = HttpStatusCode.NotAcceptable;
+                    message = exception.Message;
+                    break;
+                default:
+                    status = HttpStatusCode.InternalServerError;
+                    message = "Ocorreu um erro interno. Contatar TI";
+                    break;
+            }
+
+            ErrorDTO response = new(message);
+            context.Response.StatusCode = (int)status;
+            return context.Response.WriteAsJsonAsync(response);
+        }
+    }
+}
