70a2e6754eec1138e7330de7b009aef61a95ea94
Gabriel "Friday" Thomas <gabriel.getdev@gmail.com>
2022-09-02 21:26:16 -0300

    Feature: Cria Exception para request inexistente

---
+++ src/Escola.Domain/Exceptions/InexistenteException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Escola.Domain.Exceptions
+{
+    public class InexistenteException : Exception
+    {
+        public InexistenteException(string message) : base(message)
+        {
+        }
+    }
+}
