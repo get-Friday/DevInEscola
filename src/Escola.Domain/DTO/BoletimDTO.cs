7802089e7936a34b0fb038c337ec22cdf69bdc5e
Gabriel "Friday" Thomas <gabriel.getdev@gmail.com>
2022-09-03 10:52:52 -0300

    Feature: Cria boletimDTO

---
+++ src/Escola.Domain/DTO/BoletimDTO.cs
@@ -0,0 +1,23 @@
+﻿using Escola.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Escola.Domain.DTO
+{
+    public class BoletimDTO
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public Guid AlunoId { get; set; }
+        public BoletimDTO()
+        {
+        }
+        public BoletimDTO(Boletim boletim)
+        {
+            Id = boletim.Id;
+            AlunoId = boletim.AlunoId;
+        }
+    }
+}
