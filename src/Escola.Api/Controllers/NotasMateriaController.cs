8634650c0ac5a1a22de40c989559a0c2d22096da
Gabriel "Friday" Thomas <gabriel.getdev@gmail.com>
2022-09-03 15:36:59 -0300

    Feature: Cria arquivo NotasMateriaController

---
+++ src/Escola.Api/Controllers/NotasMateriaController.cs
@@ -0,0 +1,16 @@
+﻿using Escola.Domain.Interfaces.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Escola.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class NotasMateriaController : Controller
+    {
+        private readonly INotasMateriaServico _notasMateriaServico;
+        public NotasMateriaController(INotasMateriaServico notasMateriaServico)
+        {
+            _notasMateriaServico = notasMateriaServico;
+        }
+    }
+}
