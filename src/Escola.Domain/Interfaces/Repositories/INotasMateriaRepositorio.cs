9197faa7aa45099b9990d2a39635f28fd9186a42
Gabriel "Friday" Thomas <gabriel.getdev@gmail.com>
2022-09-03 16:00:21 -0300

    Feature: Adiciona método NotasMateriaExiste a interface

--- src/Escola.Domain/Interfaces/Repositories/INotasMateriaRepositorio.cs
+++ src/Escola.Domain/Interfaces/Repositories/INotasMateriaRepositorio.cs
@@ -13,5 +13,6 @@ namespace Escola.Domain.Interfaces.Repositories
         public void Inserir(NotasMateria notasMateria);
         public void Excluir(NotasMateria notasMateria);
         public void Alterar(NotasMateria notasMateria);
+        public bool ExisteNotasMateria(Guid id);
     }
 }
