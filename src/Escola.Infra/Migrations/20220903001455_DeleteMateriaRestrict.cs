4aeb7c95cb931382ae1a2d0a310aec0429278e94
Gabriel "Friday" Thomas <gabriel.getdev@gmail.com>
2022-09-02 21:25:55 -0300

    Feature: EF NotasMateria ao deletar nota Behavior Restrict

---
+++ src/Escola.Infra/Migrations/20220903001455_DeleteMateriaRestrict.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Escola.Infra.Migrations
+{
+    public partial class DeleteMateriaRestrict : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_NOTAS_MATERIA_MATERIA_MateriaId",
+                table: "NOTAS_MATERIA");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_NOTAS_MATERIA_MATERIA_MateriaId",
+                table: "NOTAS_MATERIA",
+                column: "MateriaId",
+                principalTable: "MATERIA",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_NOTAS_MATERIA_MATERIA_MateriaId",
+                table: "NOTAS_MATERIA");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_NOTAS_MATERIA_MATERIA_MateriaId",
+                table: "NOTAS_MATERIA",
+                column: "MateriaId",
+                principalTable: "MATERIA",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
