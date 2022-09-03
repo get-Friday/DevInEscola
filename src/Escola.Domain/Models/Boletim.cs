953c662762d4c1f8fb570c2906fe442adadeb6d6
Gabriel "Friday" Thomas <gabriel.getdev@gmail.com>
2022-09-03 11:13:00 -0300

    Feature: Implementa CTOR e método update na classe boletim

--- src/Escola.Domain/Models/Boletim.cs
+++ src/Escola.Domain/Models/Boletim.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Escola.Domain.DTO;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,5 +14,18 @@ namespace Escola.Domain.Models
 
         public Aluno Aluno { get; set; }
         public IList<NotasMateria> NotasMateria { get; set; }
+        public Boletim()
+        {
+        }
+        public Boletim(BoletimDTO boletim)
+        {
+            Id = boletim.Id;
+            AlunoId = boletim.AlunoId;
+        }
+        public void Update(BoletimDTO boletim)
+        {
+            Id = boletim.Id;
+            AlunoId = boletim.AlunoId;
+        }
     }
 }
