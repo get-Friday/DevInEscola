61e27949d5ca66320717299dfa4671499d966053
Gabriel "Friday" Thomas <gabriel.getdev@gmail.com>
2022-09-03 15:30:28 -0300

    Feature: Cria arquivo NotasMateriaServico

---
+++ src/Escola.Domain/Services/NotasMateriaServico.cs
@@ -0,0 +1,37 @@
+﻿using Escola.Domain.DTO;
+using Escola.Domain.Interfaces.Repositories;
+using Escola.Domain.Interfaces.Services;
+using Escola.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Escola.Domain.Services
+{
+    public class NotasMateriaServico : INotasMateriaServico
+    {
+        private readonly INotasMateriaRepositorio _notasMateriaRepositorio;
+        public NotasMateriaServico(INotasMateriaRepositorio notasMateriaRepositorio)
+        {
+            _notasMateriaRepositorio = notasMateriaRepositorio;
+        }
+        public NotasMateriaDTO ObterPorId(Guid guid)
+        {
+            throw new NotImplementedException();
+        }
+        public void Inserir(NotasMateriaDTO notasMateria)
+        {
+            throw new NotImplementedException();
+        }
+        public void Excluir(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+        public void Alterar(NotasMateriaDTO notasMateria)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
