c252be52e2c725aa818c381fa2c0e28682afac3e
Gabriel "Friday" Thomas <gabriel.getdev@gmail.com>
2022-09-03 11:36:29 -0300

    Fix: Migrations para DeleteBehavior do Boletim

---
+++ src/Escola.Infra/Migrations/20220903143539_DeleteBoletimRestrict.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Escola.Infra.Migrations
+{
+    public partial class DeleteBoletimRestrict : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_BOLETIM_ALUNO_AlunoId",
+                table: "BOLETIM");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_BOLETIM_ALUNO_AlunoId",
+                table: "BOLETIM",
+                column: "AlunoId",
+                principalTable: "ALUNO",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_BOLETIM_ALUNO_AlunoId",
+                table: "BOLETIM");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_BOLETIM_ALUNO_AlunoId",
+                table: "BOLETIM",
+                column: "AlunoId",
+                principalTable: "ALUNO",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
