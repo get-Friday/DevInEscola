e7ee7887d72517b2e646796c63c559b88aaf2504
Gabriel "Friday" Thomas <gabriel.getdev@gmail.com>
2022-09-06 16:59:00 -0300

    Feature: Cria controller Materias V2

---
+++ src/Escola.Api/Controllers/V2/MateriasController.cs
@@ -0,0 +1,18 @@
+﻿using Escola.Domain.DTO;
+using Escola.Domain.Interfaces.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Escola.Api.Controllers.V2
+{
+    [ApiController]
+    [ApiVersion("2.0")]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    public class MateriasController : ControllerBase
+    {
+        private readonly IMateriaServico _materiaServico;
+        public MateriasController(IMateriaServico materiaServico)
+        {
+            _materiaServico = materiaServico;
+        }
+    }
+}
