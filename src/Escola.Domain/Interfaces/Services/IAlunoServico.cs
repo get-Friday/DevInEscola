d7e7c41f6e6f8b18ff36bc635927ebc0ff1f68eb
Gabriel "Friday" Thomas <gabriel.getdev@gmail.com>
2022-09-06 16:11:31 -0300

    Feature: Passa paginacao no parâmetro ObterTodos em Interface Servico

--- src/Escola.Domain/Interfaces/Services/IAlunoServico.cs
+++ src/Escola.Domain/Interfaces/Services/IAlunoServico.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Escola.Domain.DTO;
+using Escola.Domain.Models;
 
 namespace Escola.Domain.Interfaces.Services
 {
     public interface IAlunoServico
     {
-        IList<AlunoDTO> ObterTodos();
+        IList<AlunoDTO> ObterTodos(Paginacao paginacao);
         AlunoDTO ObterPorId(Guid id);
         void Inserir(AlunoDTO aluno);
         void Excluir (Guid id);
