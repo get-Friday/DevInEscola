3603aaa8389df74da51edcfc0c4d6da5d676cf3f
Gabriel "Friday" Thomas <gabriel.getdev@gmail.com>
2022-09-06 16:09:50 -0300

    Feature: Cria modelo paginacao

---
+++ src/Escola.Domain/Models/Paginacao.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Escola.Domain.Models
+{
+    public class Paginacao
+    {
+        public int Take { get; set; }
+        public int Skip { get; set; }
+        public Paginacao(int take, int skip)
+        {
+            Take = take;
+            Skip = skip;
+        }
+    }
+}
