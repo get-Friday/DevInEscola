2366d1f8f3eaa9d8966cc54fcaaa3a2468ebdede
Gabriel "Friday" Thomas <gabriel.getdev@gmail.com>
2022-09-07 19:22:28 -0300

    Feature: Implementa arquivo configuração para versionar Swagger

---
+++ src/Escola.Api/Config/ConfigureSwaggerGenOptions.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace Escola.Api.Config
+{
+    public class ConfigureSwaggerGenOptions : IConfigureOptions<SwaggerGenOptions>
+    {
+        private readonly IApiVersionDescriptionProvider _provider;
+        public ConfigureSwaggerGenOptions(IApiVersionDescriptionProvider provider)
+        {
+            _provider = provider;
+        }
+        public void Configure(SwaggerGenOptions options)
+        {
+            foreach (var description in _provider.ApiVersionDescriptions)
+            {
+                options.SwaggerDoc(description.GroupName, CreateOpenApiInfo(description));
+            }
+        }
+        private static OpenApiInfo CreateOpenApiInfo(ApiVersionDescription description)
+        {
+            var info = new OpenApiInfo()
+            {
+                Title = "Escola.API",
+                Version = description.ApiVersion.ToString()
+            };
+
+            if (description.IsDeprecated)
+            {
+                info.Description += " (deprecated)";
+            }
+
+            return info;
+        }
+    }
+}
