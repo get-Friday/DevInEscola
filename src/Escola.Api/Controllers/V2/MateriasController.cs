774aafec8804e7356f4e48c58ac5ac4ff79afb22
Gabriel "Friday" Thomas <gabriel.getdev@gmail.com>
2022-09-08 15:46:55 -0300

    Feature: Implementa Alterar Materia V2 usando mesmo service

--- src/Escola.Api/Controllers/V2/MateriasController.cs
+++ src/Escola.Api/Controllers/V2/MateriasController.cs
@@ -46,5 +46,21 @@ namespace Escola.Api.Controllers.V2
             _materiaServico.Inserir(materiaV1);
             return StatusCode(StatusCodes.Status201Created);
         }
+        [MapToApiVersion("2.0")]
+        [HttpPut("{id}")]
+        public IActionResult Alterar(
+            [FromRoute] Guid id,
+            [FromBody] MateriaDTO materia
+        )
+        {
+            materia.Id = id;
+            Domain.DTO.V1.MateriaDTO materiaV1 = new()
+            {
+                Id = materia.Id,
+                Nome = materia.Disciplina
+            };
+            _materiaServico.Alterar(materiaV1);
+            return StatusCode(StatusCodes.Status201Created);
+        }
     }
 }
