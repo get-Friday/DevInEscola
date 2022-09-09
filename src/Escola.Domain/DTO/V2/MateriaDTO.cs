0f695d6385dc1b4d31911c41087f72044099a5de
Gabriel "Friday" Thomas <gabriel.getdev@gmail.com>
2022-09-09 15:59:43 -0300

    Feature: Remove construtor MateriaDTO v2 que agora é papel do automapper

--- src/Escola.Domain/DTO/V2/MateriaDTO.cs
+++ src/Escola.Domain/DTO/V2/MateriaDTO.cs
@@ -4,13 +4,5 @@
     {
         public Guid Id { get; set; }
         public string Disciplina { get; set; }
-        public MateriaDTO()
-        {
-        }
-        public MateriaDTO(V1.MateriaDTO materia)
-        {
-            Id = materia.Id;
-            Disciplina = materia.Nome;
-        }
     }
 }
