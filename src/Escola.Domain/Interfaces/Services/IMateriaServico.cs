5c9951cc32d53e83a2be834a4ec424b593fcd3d7
Gabriel "Friday" Thomas <gabriel.getdev@gmail.com>
2022-09-09 09:44:58 -0300

    Feature: Passa paginacao para ObterTodos e cria Obter total na interface servico materia

--- src/Escola.Domain/Interfaces/Services/IMateriaServico.cs
+++ src/Escola.Domain/Interfaces/Services/IMateriaServico.cs
@@ -1,15 +1,16 @@
 ﻿using Escola.Domain.DTO.V1;
+using Escola.Domain.Models;
 
 namespace Escola.Domain.Interfaces.Services
 {
     public interface IMateriaServico
     {
-        IList<MateriaDTO> ObterTodos();
+        IList<MateriaDTO> ObterTodos(Paginacao paginacao);
         MateriaDTO ObterPorId(Guid id);
         IList<MateriaDTO> ObterPorNome(string nome);
         void Inserir(MateriaDTO materia);
         void Excluir(Guid id);
         void Alterar(MateriaDTO materia);
-
+        int ObterTotal();
     }
 }
