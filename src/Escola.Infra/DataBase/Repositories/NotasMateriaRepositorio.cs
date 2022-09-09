d33d131dd7340921745d3ad7ada8218273e95553
Gabriel "Friday" Thomas <gabriel.getdev@gmail.com>
2022-09-09 09:36:22 -0300

    Feature: Implementa BaseRepositorio para NotasMateriaRepositorio

--- src/Escola.Infra/DataBase/Repositories/NotasMateriaRepositorio.cs
+++ src/Escola.Infra/DataBase/Repositories/NotasMateriaRepositorio.cs
@@ -3,31 +3,10 @@ using Escola.Domain.Models;
 
 namespace Escola.Infra.DataBase.Repositories
 {
-    public class NotasMateriaRepositorio : INotasMateriaRepositorio
+    public class NotasMateriaRepositorio : BaseRepositorio<NotasMateria, Guid>, INotasMateriaRepositorio
     {
-        private readonly EscolaDBContexto _contexto;
-        public NotasMateriaRepositorio(EscolaDBContexto contexto)
+        public NotasMateriaRepositorio(EscolaDBContexto contexto) : base(contexto)
         {
-            _contexto = contexto;
-        }
-        public NotasMateria ObterPorId(Guid id)
-        {
-            return _contexto.NotasMateria.Find(id);
-        }
-        public void Inserir(NotasMateria notasMateria)
-        {
-            _contexto.NotasMateria.Add(notasMateria);
-            _contexto.SaveChanges();
-        }
-        public void Excluir(NotasMateria notasMateria)
-        {
-            _contexto.NotasMateria.Remove(notasMateria);
-            _contexto.SaveChanges();
-        }
-        public void Alterar(NotasMateria notasMateria)
-        {
-            _contexto.NotasMateria.Update(notasMateria);
-            _contexto.SaveChanges();
         }
         public bool ExisteNotasMateria(Guid id)
         {
