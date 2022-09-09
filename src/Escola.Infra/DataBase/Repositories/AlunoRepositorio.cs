96b39b89e5f3ca456a9a5dcd8e99c93a0bb8c0cc
Gabriel "Friday" Thomas <gabriel.getdev@gmail.com>
2022-09-09 09:34:04 -0300

    Feature: Implementa BaseRepositorio para AlunoRepositorio

--- src/Escola.Infra/DataBase/Repositories/AlunoRepositorio.cs
+++ src/Escola.Infra/DataBase/Repositories/AlunoRepositorio.cs
@@ -3,46 +3,14 @@ using Escola.Domain.Models;
 
 namespace Escola.Infra.DataBase.Repositories
 {
-    public class AlunoRepositorio : IAlunoRepositorio
+    public class AlunoRepositorio : BaseRepositorio<Aluno, Guid>, IAlunoRepositorio
     {
-        private readonly EscolaDBContexto _contexto;
-        public AlunoRepositorio(EscolaDBContexto contexto)
+        public AlunoRepositorio(EscolaDBContexto contexto) : base(contexto)
         {
-            _contexto = contexto;
         }
-
-        public void Alterar(Aluno aluno)
-        {
-            _contexto.Alunos.Update(aluno);
-            _contexto.SaveChanges();
-        }
-
-        public void Excluir(Aluno aluno)
-        {
-            _contexto.Alunos.Remove(aluno);
-            _contexto.SaveChanges();
-        }
-
         public bool ExisteMatricula(int matricula)
         {
             return _contexto.Alunos.Any(a => a.Matricula == matricula);
-        }
-
-        public void Inserir(Aluno aluno)
-        {
-            _contexto.Alunos.Add(aluno);
-            _contexto.SaveChanges();
-        }
-
-        public Aluno ObterPorId(Guid id)
-        {
-            return _contexto.Alunos.Find(id);
-        }
-        public IEnumerable<Aluno> ObterTodos(Paginacao paginacao)
-        {
-            return _contexto.Alunos
-                .Take(paginacao.Take)
-                .Skip(paginacao.Skip);
         }
         public int ObterTotal()
         {
