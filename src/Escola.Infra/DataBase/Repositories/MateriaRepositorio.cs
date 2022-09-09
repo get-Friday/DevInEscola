7f3b47b14327a7f4e4be9acf47dd69436e007a7b
Gabriel "Friday" Thomas <gabriel.getdev@gmail.com>
2022-09-09 09:37:34 -0300

    Feature: Implementa BaseRepositorio para MateriaRepositorio

--- src/Escola.Infra/DataBase/Repositories/MateriaRepositorio.cs
+++ src/Escola.Infra/DataBase/Repositories/MateriaRepositorio.cs
@@ -3,41 +3,15 @@ using Escola.Domain.Models;
 
 namespace Escola.Infra.DataBase.Repositories
 {
-    public class MateriaRepositorio : IMateriaRepositorio
+    public class MateriaRepositorio : BaseRepositorio<Materia, Guid>, IMateriaRepositorio
     {
-        private readonly EscolaDBContexto _contexto;
-        public MateriaRepositorio(EscolaDBContexto contexto)
+        public MateriaRepositorio(EscolaDBContexto contexto) : base(contexto)
         {
-            _contexto = contexto;
-        }
-
-        public IEnumerable<Materia> ObterTodos()
-        {
-            return _contexto.Materias;
-        }
-        public Materia ObterPorId(Guid id)
-        {
-            return _contexto.Materias.Find(id);
         }
         public IEnumerable<Materia> ObterPorNome(string nome)
         {
             return _contexto.Materias
                 .Where(m => m.Nome.Contains(nome));
-        }
-        public void Inserir(Materia materia)
-        {
-            _contexto.Materias.Add(materia);
-            _contexto.SaveChanges();
-        }
-        public void Excluir(Materia materia)
-        {
-            _contexto.Materias.Remove(materia);
-            _contexto.SaveChanges();
-        }
-        public void Alterar(Materia materia)
-        {
-            _contexto.Materias.Update(materia);
-            _contexto.SaveChanges();
         }
         public bool ExisteMateria(string nome)
         {
