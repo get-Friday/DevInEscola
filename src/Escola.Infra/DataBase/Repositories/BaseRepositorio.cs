304e7624f17caa77a8d8daaf7db36aa44822d0c1
Gabriel "Friday" Thomas <gabriel.getdev@gmail.com>
2022-09-09 09:30:43 -0300

    Feature: Implementa classe BaseRepositorio

---
+++ src/Escola.Infra/DataBase/Repositories/BaseRepositorio.cs
@@ -0,0 +1,43 @@
+﻿using Escola.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Escola.Infra.DataBase.Repositories
+{
+    public class BaseRepositorio <TEntity, TKey> where TEntity : class
+    {
+        private readonly EscolaDBContexto _contexto;
+        public BaseRepositorio(EscolaDBContexto contexto)
+        {
+            _contexto = contexto;
+        }
+        public IEnumerable<TEntity> ObterTodos(Paginacao paginacao)
+        {
+            return _contexto.Set<TEntity>()
+                .Take(paginacao.Take)
+                .Skip(paginacao.Skip);
+        }
+        public TEntity ObterPorId(TKey key)
+        {
+            return _contexto.Set<TEntity>().Find(key);
+        }
+        public void Inserir(TEntity entity)
+        {
+            _contexto.Set<TEntity>().Add(entity);
+            _contexto.SaveChanges();
+        }
+        public void Alterar(TEntity entity)
+        {
+            _contexto.Set<TEntity>().Update(entity);
+            _contexto.SaveChanges();
+        }
+        public void Excluir(TEntity entity)
+        {
+            _contexto.Set<TEntity>().Remove(entity);
+            _contexto.SaveChanges();
+        }
+    }
+}
